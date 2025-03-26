d0ce29acb46c7a05b633cce82e9bfd36016aa688
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2025-03-26 18:58:58 +0300

    added cors to be

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,24 @@ const cors = require('cors');
 const denklikRoutes = require('./routes/denklik');
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://medical-compliance-app-1.onrender.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json({ type: 'application/json' }));  // UTF-8 çözümleme için
 
 app.use('/api', denklikRoutes);
